86fd63af11e8df4aba73514c6c3ed722b19ab3d5
Dan <danaaboston@gmail.com>
2022-11-03 21:07:47 -0400

    Attempting to set up options for computer to choose from and started setting up prompts

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 // Assignment code here
-
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "]", ";", ":", "'", '"', "|", ",", "<", ".", ">", "/", "?", "`", "~"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
   var length = window.prompt("How many characters long should the password be?")
-  var uppercase = window.prompt("Do you want to include uppercase characters? (Y/N)");
-  var lowercase = window.prompt ("Do you want to include lowercase characters? (Y/N)");
-  var numbers = window.prompt ("Do you want to include numbers? (Y/N)");
-  var special = window.prompt ("Do you want to include special characters? (Y/N)");
+  var uppercaseChoice = window.prompt("Do you want to include uppercase characters? (Y/N)");
+  var lowercaseChoice = window.prompt ("Do you want to include lowercase characters? (Y/N)");
+  var numbersChoice = window.prompt ("Do you want to include numbers? (Y/N)");
+  var specialChoice = window.prompt ("Do you want to include special characters? (Y/N)");
 
-  console.log(length);
-  console.log(uppercase);
-  console.log(lowercase);
-  console.log(numbers);
-  console.log(special);
-
+if (length > 8 && length < 128) {
+  console.log("Continue")
+} else {
+  console.log ("Failure")
+}
   return "Generated Password Test";
 }
 
