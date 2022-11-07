40f8af3eff6899b079ed57b53c4de4dbc76d3d48
Dan <danaaboston@gmail.com>
2022-11-07 02:05:41 -0500

    Added notes to javascript to further explain functionality

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment code here
-// All possible characters from each userInput choice are stored here.
+// All possible characters from each userInput choice are stored here
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -46,11 +46,13 @@ function randomize(x) {
 
 // Generate password function
 function generatePassword() {
+  // Start by declaring variables
   var choice = userInput();
   var selectedCharacters = [];
   var criteriaCheck = [];
   var finalPassword = [];
 
+  // Go through and run all possible character types into the possible finalPassword
   if (choice.uppercaseChoice) {
     selectedCharacters = selectedCharacters.concat(uppercase);
     criteriaCheck.push(randomize(uppercase));
@@ -71,6 +73,7 @@ function generatePassword() {
     criteriaCheck.push(randomize(special));
   }
 
+  // Forming of the finalPassword happens here
   for (var i = 0; i < choice.length; i++) {
     var randomCharacter = randomize(selectedCharacters);
     finalPassword.push(randomCharacter);
@@ -94,3 +97,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Note for myself to look back on, hopefully I don't lose points for this! If this is not okay please comment in the grade and let me know :)
+// The user first clicks the button, which runs the writePassword function.
+// That then leads the program to run the generatePassword function in order to fill the 'password' variable.
+// The generatePassword function then runs the userInput function in order to fill the 'choice' variable with the returned 'userSelection' object.
+// The 'userInput' function adds the user's final password properties, and verifies that their input is a valid request.
+// The 'userSelection' object is filled with properties containing the results of the user's inputs to the prompts.
+// Back at the generatePassword function, 'choice' is now the same as 'userSelection' and the properties can now be accessed through the 'choice' var.
+// Then the if statements begin running inside of generatePassword, testing if the user selected those options. If not, that statement is skipped.
+// While the choices are being checked, the selectedCharacters var is combing all of the possible characters from the selected types into a single array.
+// At the same time, it is adding that a single random character from the respective character type to another array called criteriaCheck.
+// Once all of the choices have been checked for, a for statement runs and pushes one random character from the selectedCharacters array, 
+// which now contains all possible characters, using the 'randomize' function, into the finalPassword array.
+// After that is done, the next for statement then replaces the respective character within the finalPassword array with the random character set
+// earlier that is now stored in the criteriaCheck array.
+// Now the finalPassword is changed from an array to a string using the .join() method.
+// Back in the writePassword function the 'password' variable now = finalPassword.
+// The javascript now calls the #password ID from the HTML and replaces the text within that field from "Your Secure Password" with the
+// final generated password.
